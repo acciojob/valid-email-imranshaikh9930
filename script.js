61ebbe809929c6283a4e94b6344137fd6709a3a2
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-11-16 21:29:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function validEmail(str) {
   //your JS code here.
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  // Check if the email is not empty and matches the regular expression
+  return str !== '' && emailRegex.test(str);
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
+const str = prompt("Enter an email address.");;
 alert(validEmail(str));
